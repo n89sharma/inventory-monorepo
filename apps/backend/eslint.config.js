2bc56ec6e7f54f502479c2268a844ee3373731fa
Nikhil Sharma <n89sharma@gmail.com>
2026-06-24 10:31:47 -0400

    linting with sonarjs added

---
+++ apps/backend/eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import sonarjs from 'eslint-plugin-sonarjs'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist', 'generated'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      sonarjs.configs.recommended,
+    ],
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.node,
+    },
+  },
+)
--- apps/frontend/eslint.config.js
+++ apps/frontend/eslint.config.js
@@ -2,12 +2,17 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import sonarjs from 'eslint-plugin-sonarjs'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      sonarjs.configs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
